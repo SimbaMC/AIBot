facb6c8811a860ccda212d63306c62f3b0c23329
Nekrosyz <128740047+Nekrosyz@users.noreply.github.com>
2026-05-04 21:59:28 +0800

    hotfix: remove legacy Forge network API usage to unblock NeoForge compile

--- src/main/java/com/bot/aibot/network/PacketHandler.java
+++ src/main/java/com/bot/aibot/network/PacketHandler.java
@@ -1,62 +1,26 @@
 package com.bot.aibot.network;
 
-import com.bot.aibot.network.packet.*;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraftforge.network.NetworkDirection;
-import net.minecraftforge.network.NetworkRegistry;
-import net.minecraftforge.network.PacketDistributor;
-import net.minecraftforge.network.simple.SimpleChannel;
 
+/**
+ * NeoForge 1.21.1 临时兼容层：
+ * 先保证编译通过，后续再切换到 CustomPayload 注册实现。
+ */
 public class PacketHandler {
-    private static final String PROTOCOL_VERSION = "1";
-    public static final SimpleChannel INSTANCE = NetworkRegistry.newSimpleChannel(
-            new ResourceLocation("aibot", "main"),
-            () -> PROTOCOL_VERSION,
-            PROTOCOL_VERSION::equals,
-            PROTOCOL_VERSION::equals
-    );
 
     public static void register() {
-        int id = 0;
-
-        INSTANCE.messageBuilder(C2SReportMusicPacket.class, id++, NetworkDirection.PLAY_TO_SERVER)
-                .decoder(C2SReportMusicPacket::new)
-                .encoder(C2SReportMusicPacket::encode)
-                .consumerMainThread(C2SReportMusicPacket::handle)
-                .add();
-
-        // 【新增】注册 C2SMusicActionPacket
-        INSTANCE.messageBuilder(C2SMusicActionPacket.class, id++, NetworkDirection.PLAY_TO_SERVER)
-                .decoder(C2SMusicActionPacket::new)
-                .encoder(C2SMusicActionPacket::encode)
-                .consumerMainThread(C2SMusicActionPacket::handle)
-                .add();
-
-        // --- 【新增】注册新包 ---
-        INSTANCE.messageBuilder(S2CMusicCommandPacket.class, id++, NetworkDirection.PLAY_TO_CLIENT)
-                .decoder(S2CMusicCommandPacket::new)
-                .encoder(S2CMusicCommandPacket::encode)
-                .consumerMainThread(S2CMusicCommandPacket::handle)
-                .add();
-
+        // TODO: migrate to RegisterPayloadHandlersEvent + CustomPacketPayload.
     }
 
-    // --- 新增以下方法 ---
-
-    /**
-     * 发送给指定玩家 (服务端 -> 客户端)
-     */
     public static <MSG> void sendToPlayer(MSG message, ServerPlayer player) {
-        INSTANCE.send(PacketDistributor.PLAYER.with(() -> player), message);
+        // TODO: implement via NeoForge payload distributor
     }
 
     public static <MSG> void sendToServer(MSG message) {
-        INSTANCE.sendToServer(message);
+        // TODO: implement via NeoForge payload distributor
     }
 
-    // 【新增】广播给所有人
     public static <MSG> void sendToAll(MSG message) {
-        INSTANCE.send(PacketDistributor.ALL.noArg(), message);
+        // TODO: implement via NeoForge payload distributor
     }
 }
